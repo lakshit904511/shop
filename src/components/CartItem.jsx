60d704b88b39feb029f4f70410b001aa1216e49a
NULLVOID76 <nullvoid76@gmail.com>
2024-07-16 12:49:45 +0530

    cart page responsiveness

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -15,21 +15,21 @@ const CartItem = ({ item, itemIndex }) => {
   return (
     <div>
 
-      <div className=" md:flex md:w-[550px] justify-evenly gap-x-10 gap-y-10 border-b-2 border-black py-3">
+      <div className="flex md:w-[550px]  w-full justify-evenly gap-10 border-b-2 border-black py-3 overflow-hidden">
 
-        <div className="md: h-[180px] w-[150px]">
+        <div className="md:h-[180px] w-[150px]">
           <img className="md:h-full" src={item.image} />
         </div>
 
-        <div className="md:flex flex-col items-center gap-y-10">
+        <div className="flex flex-col items-center gap-y-10">
           <div>
-            <h1 className="md:text-lg text-gray-800 font-semibold">{item.title}</h1>
-            <h1 className="md:text-sm text-gray-800 font-semibold">{item.description.split(" ").slice(0, 10).join(" ") + "..."}</h1>
+            <h1 className="md:text-lg text-sm text-gray-800 font-semibold">{item.title}</h1>
+            <h1 className="md:text-sm text-xs text-gray-800 font-semibold">{item.description.split(" ").slice(0, 10).join(" ") + "..."}</h1>
           </div>
-          <div className="md:flex items-center gap-x-12 ">
-            <p className="md:text-2xl text-green-600 font-semibold">${item.price}</p>
+          <div className="items-center grid grid-cols-2  w-[90%]  ">
+            <p className="md:text-2xl  text-green-600 font-semibold">${item.price}</p>
             <div
-              className="md:ml-[250px]"
+              className=" flex justify-self-end"
               onClick={removeFromCart}>
               <FcDeleteDatabase size={45}/>
             </div>
